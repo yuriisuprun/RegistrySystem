e9acaf92b65de00138925f02650c529337108cfc
Yurii Suprun <iursuprun@gmail.com>
2020-04-28 23:22:12 +0300

    SpringBootPart: Add service layer

---
+++ src/main/java/com/suprun/registrysystem/services/BikeService.java
@@ -0,0 +1,24 @@
+package com.suprun.registrysystem.services;
+
+import com.suprun.registrysystem.models.Bike;
+import com.suprun.registrysystem.repositories.BikeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Service
+public class BikeService {
+
+    @Autowired
+    private BikeRepository bikeRepository;
+
+    public List<Bike> getBikes(){
+        return bikeRepository.findAll();
+    }
+
+    public Bike get(long id){
+        return bikeRepository.getOne(id);
+    }
+}
