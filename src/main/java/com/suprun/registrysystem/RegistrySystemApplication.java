9e2499b70991006ab01d42a60e65dcbac8533d21
Yurii Suprun <Yurii_Suprun@epam.com>
2020-04-28 18:43:12 +0300

    Spring Boot initializing

---
+++ src/main/java/com/suprun/registrysystem/RegistrySystemApplication.java
@@ -0,0 +1,13 @@
+package com.suprun.registrysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suprun/registrysystem/RegistrySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suprun.registrysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
